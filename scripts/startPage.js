206f177765dac70e163b0cee21685689770a771b
Sviatoslav-Zubrytskyi <sviatoslav.zubrytskyi@student.nhlstenden.com>
2023-10-16 17:02:24 +0200

    startPage.js

---
+++ scripts/startPage.js
@@ -0,0 +1,6 @@
+buttonStart = document.createElement("button");
+buttonStart.type="button";
+buttonStart.addEventListener("click", startTest);
+buttonStart.innerHTML="Start Real Exam";
+buttonStart.style = "margin:0;position:absolute;top:30%;transform: translateY(-50%);transform: translateX(-50%);left:50%;background-color:white;width:300px;height:60px;cursor:pointer"
+document.body.appendChild(buttonStart);
