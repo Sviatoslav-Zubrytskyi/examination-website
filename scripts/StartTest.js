e53aef82b71dc792aded60faa0ccf07fc27b3157
Sviatoslav-Zubrytskyi <sviatoslav.zubrytskyi@student.nhlstenden.com>
2023-10-16 20:29:19 +0200

    planning further development

--- scripts/StartTest.js
+++ scripts/StartTest.js
@@ -3,3 +3,13 @@ const TestPage0 = new TestPageGen(1);
 function startTest() {
     TestPage0.newTestPage();
 }
+
+// forEach question in array questions -> generate new page
+
+// each page gets unique id number for easier navigation. Navigation bar on the left could be added later
+
+// each page's answers are generated in random order and each question has only 1 correct answer which should be declared later. 
+//After pressing Answer button, style of the questions should be changed depended on wether you are right or not
+
+// functions goBack and goForward should go to the previous page which should be saved. After each reload of the page all is generated from scratch. Saving could be added later
+
